c93ca642d4daaf065816073b1a3c5042da2845e0
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2024-03-22 16:09:14 -0300

    adding progress bar

--- src/contract/contractComponents/Sections.jsx
+++ src/contract/contractComponents/Sections.jsx
@@ -1,27 +1,50 @@
 import React from "react";
+import { useState } from "react";
+import ProgressBar from "react-bootstrap/ProgressBar";
 
 export const Sections = ({ information, handleInputForm, form }) => {
+  const numberOfSections = information?.sections?.length;
+  let unitPercentage = 100 / numberOfSections;
+
+  const [progress, setProgress] = useState(
+    form?.section
+      ? (form.section - 1) * unitPercentage + unitPercentage
+      : unitPercentage
+  );
+
+  const handleButtonClick = (section) => {
+    const targetProgress = (section - 1) * unitPercentage + unitPercentage;
+    setProgress(targetProgress);
+    handleInputForm({ section });
+  };
+
   return (
-    <div style={{ display: "flex", marginBottom: "20px" }}>
-      {information?.sections?.map((sec) => {
-        return (
-          <div key={`${sec.key}header`}>
-            <button
-              style={{
-                margin: "10px",
-                padding: "5px 10px",
-                borderRadius: "5px",
-                border: "none",
-                backgroundColor: form?.section === sec?.position && "lightblue",
-                pointerEvents: form?.section === sec?.position && "none",
-              }}
-              onClick={() => handleInputForm({ section: sec?.position })}
-            >
-              {sec.key}
-            </button>
-          </div>
-        );
-      })}
+    <div>
+      <div style={{ display: "flex", marginBottom: "5px" }}>
+        {information?.sections?.map((sec) => {
+          return (
+            <div key={`${sec.key}header`}>
+              <button
+                style={{
+                  margin: "10px",
+                  padding: "5px 10px",
+                  borderRadius: "5px",
+                  border: "none",
+                  backgroundColor:
+                    form?.section === sec?.position && "lightblue",
+                  pointerEvents: form?.section === sec?.position && "none",
+                }}
+                onClick={() => {
+                  handleButtonClick(sec.position);
+                }}
+              >
+                {sec.key}
+              </button>
+            </div>
+          );
+        })}
+      </div>
+      <ProgressBar now={progress} />
     </div>
   );
 };
